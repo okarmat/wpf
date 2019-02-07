60b3bb4162217bbffe462b7fb8d0e267220c1a7e
Tom <tomek.okarma@gmail.com>
2019-02-07 20:30:07 +0100

    first steps using Map Suit for WPF library

---
+++ HelloWorldThinkgeo/HelloWorldThinkgeo/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using ThinkGeo.MapSuite.Wpf;
+
+namespace HelloWorldThinkgeo
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            WpfMap.MapUnit = ThinkGeo.MapSuite.GeographyUnit.DecimalDegree;
+            WpfMap.CurrentExtent = new ThinkGeo.MapSuite.Shapes.RectangleShape(20.030486, 50.00289, 20.03997, 49.998476);
+
+            WpfMap.Overlays.Add(new WorldStreetsAndImageryOverlay());
+            WpfMap.Refresh();
+        }
+    }
+}
